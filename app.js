5327ac507549923d1c83d02084662b4e300dc748
Jeff Torres <jeffrey.n.torres@gmail.com>
2016-04-24 09:37:38 -0700

    Added basic objects

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-var object {
-  prop1: '';
-  prop2: '';
+var pikePlace = {
+  minCust: '17';
+  maxCust: '18';
+  avgSale: '5.2';
+}
+
+var seaTac = {
+  minCust: '6';
+  maxCust: '24';
+  avgSale: '1.2';
+}
+
+var southcenter = {
+  minCust: '11';
+  maxCust: '38';
+  avgSale: '1.9';
+}
+
+var bellevue = {
+  minCust: '20';
+  maxCust: '48';
+  avgSale: '3.3';
+}
+
+var alki = {
+  minCust: '3';
+  maxCust: '24';
+  avgSale: '2.6';
 }
