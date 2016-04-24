45ccc07613d58ef1bb95dd203a5357294bc91bfc
Jeff Torres <jeffrey.n.torres@gmail.com>
2016-04-24 10:22:09 -0700

    added rndm cust generator to all objects

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
 var pikePlace = {
-  minCust: '17';
-  maxCust: '18';
-  avgSale: '5.2';
+  minCust: '17',
+  maxCust: '18',
+  avgSale: '5.2'
+  randCust: function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
 }
 
 var seaTac = {
   minCust: '6';
   maxCust: '24';
   avgSale: '1.2';
+  randCust: function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
 }
 
 var southcenter = {
   minCust: '11';
   maxCust: '38';
   avgSale: '1.9';
+  randCust: function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
 }
 
 var bellevue = {
   minCust: '20';
   maxCust: '48';
   avgSale: '3.3';
+  randCust: function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
 }
 
 var alki = {
   minCust: '3';
   maxCust: '24';
   avgSale: '2.6';
+  randCust: function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
 }
