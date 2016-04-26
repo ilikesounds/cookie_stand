ec03c10575f8160df7d2acb215940a75ed98af42
Jeff Torres <jeffrey.n.torres@gmail.com>
2016-04-25 17:03:47 -0700

    Moved all stores into store object

--- app.js
+++ app.js
@@ -1,50 +1,57 @@
-var pikePlace = {
+var stores = [
+
+  {
+  name: 'Pike Place',
   minCust: '17',
   maxCust: '18',
   avgSale: '5.2'
-  randCust: function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
   },
-  render: function() {
-    var liEl = document.createElement('li');
-    var ulEl = document.createElement('ul');
-    liEl.textContent = this.minCust;
-    liEl.textContent =
+
+  {
+  name: 'SeaTac',
+  minCust: '6',
+  maxCust: '24',
+  avgSale: '1.2',
+  },
+
+  {
+  name: 'Southcenter',
+  minCust: '11',
+  maxCust: '38',
+  avgSale: '1.9',
+  },
+
+  {
+  name: 'Bellevue',
+  minCust: '20',
+  maxCust: '48',
+  avgSale: '3.3',
+  },
+
+  {
+  name: 'Alki',
+  minCust: '3',
+  maxCust: '24',
+  avgSale: '2.6',
+  }
+]
+
+stores.randCust =  function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+stores.render = function() {
+  var kV = Object.keys(this);
+  var newUl = document.createElement('ul');
+  for (var i = 0; i < kV.length; i++) {
+    var newLi = document.createElement('li');
+    newUl.appendChild(newLi);
+    var newText = document.createTextNode(kV[i]);
   }
 }
 
-var seaTac = {
-  minCust: '6';
-  maxCust: '24';
-  avgSale: '1.2';
-  randCust: function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  },
+for (store in stores) {
+  stores.render();
 }
 
-var southcenter = {
-  minCust: '11';
-  maxCust: '38';
-  avgSale: '1.9';
-  randCust: function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  },
-}
-
-var bellevue = {
-  minCust: '20';
-  maxCust: '48';
-  avgSale: '3.3';
-  randCust: function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  },
-}
-
-var alki = {
-  minCust: '3';
-  maxCust: '24';
-  avgSale: '2.6';
-  randCust: function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  },
-}
+stores.render();
