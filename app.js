ad66f2df2aa7da20dcf0123d35389a35bb614cc4
Jeff Torres <jeffrey.n.torres@gmail.com>
2016-05-03 23:32:53 -0700

    Created xy axis creation function.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+var sD = document.getElementById('storesDiv');
+
 var hoursOpen = [
-  '10:00 am: ',
-  '11:00 am: ',
-  '12:00 pm: ',
-  '01:00 pm: ',
-  '02:00 pm: ',
-  '03:00 pm: ',
-  '04:00 pm: ',
-  '05:00 pm: '
+  '10:00 am',
+  '11:00 am',
+  '12:00 pm',
+  '01:00 pm',
+  '02:00 pm',
+  '03:00 pm',
+  '04:00 pm',
+  '05:00 pm'
 ];
 
 function Store(name, minCust, maxCust, avgCookie, sectionId) {
@@ -34,24 +36,76 @@ Store.prototype.hourTotal = function(hTotal, dTotal, avg, rand) {
   return dTotal;
 };
 
-Store.prototype.render = function(sectId) {
-  console.log(this.hrTotal);
-  this.dayTotal = this.hourTotal(this.hrTotal, this.dayTotal, this.avgCookie,
-    this.randCust);
-  var pEl = document.getElementById(sectId);
-  var ulEl = document.createElement('ul');
+//function() {
+//  var createdTD = [];
+//  for (tr = 0; tr < 5; tr++) {
+//    var ctd = document.createElement('td');
+//    createdTD.push(ctd);
+//  }
+//};
 
-  for (var i = 0; i < hoursOpen.length; i++) {
-    var liEl = document.createElement('li');
-    pEl.textContent = this.name;
-    liEl.textContent = hoursOpen[i] + this.hrTotal[i];
-    ulEl.appendChild(liEl);
+// How can I DRY this out using a for loop to automate column creation based on the first 4 indicies of the store object
+function renderTH() {
+  var tHeader = document.createElement('thead');
+  var tBody = document.createElement('tbody');
+  tBody.id = 'tableBody';
+  var tHeaderRow = document.createElement('th');
+  var tHD1 = document.createElement('td');
+  var tHD2 = document.createElement('td');
+  var tHD3 = document.createElement('td');
+  var tHD4 = document.createElement('td');
+  var tHD5 = document.createElement('td');
+  tHD1.textContent = '';
+  tHD2.textContent = 'Store Name';
+  tHD3.textContent = 'Min. Cust./Hr.';
+  tHD4.textContent = 'Max. Cust./Hr.';
+  tHD5.textContent = 'Avg. Cookie/Hr.';
+  tHeaderRow.appendChild(tHD1);
+  tHeaderRow.appendChild(tHD2);
+  tHeaderRow.appendChild(tHD3);
+  tHeaderRow.appendChild(tHD4);
+  tHeaderRow.appendChild(tHD5);
+  tHeaderRow.id = 'thRow';
+  tHeader.appendChild(tHeaderRow);
+  sD.appendChild(tHeader);
+  sD.appendChild(tBody);
+  for (var hr = 0; hr < hoursOpen.length; hr++) {
+    var thr = document.createElement('tr');
+    thr.textContent = hoursOpen[hr];
+    tBody.appendChild(thr);
   }
-  var liMent = document.createElement('li');
-  liMent.textContent = 'Totals: ' + this.dayTotal;
-  ulEl.appendChild(liMent);
-  pEl.appendChild(ulEl);
 };
+
+// Store.prototype.render = function(sectId) {
+//   console.log(this.hrTotal);
+//   this.dayTotal = this.hourTotal(this.hrTotal, this.dayTotal, this.avgCookie,
+//     this.randCust);
+//   var trEl = document.createElement('tr');
+//
+//   for (var i = 0; i < hoursOpen.length; i++) {
+//     var tdEl = document.createElement('tr');
+//     pEl.textContent = this.name;
+//     liEl.textContent = hoursOpen[i] + this.hrTotal[i];
+//     ulEl.appendChild(liEl);
+//
+// };
+// Store.prototype.render = function(sectId) {
+//   console.log(this.hrTotal);
+//   this.dayTotal = this.hourTotal(this.hrTotal, this.dayTotal, this.avgCookie, this.randCust);
+//   var pEl = document.getElementById(sectId);
+//   var ulEl = document.createElement('ul');
+//
+//   for (var i = 0; i < hoursOpen.length; i++) {
+//     var liEl = document.createElement('li');
+//     pEl.textContent = this.name;
+//     liEl.textContent = hoursOpen[i] + this.hrTotal[i];
+//     ulEl.appendChild(liEl);
+//   }
+//   var liMent = document.createElement('li');
+//   liMent.textContent = 'Totals: ' + this.dayTotal;
+//   ulEl.appendChild(liMent);
+//   pEl.appendChild(ulEl);
+// };
 
 var pike = new Store('Pike Place Store', 17, 88, 5.2, 'pike');
 var sea = new Store('SeaTac Store', 6, 24, 1.2, 'seaTac');
@@ -67,6 +121,8 @@ var storeName = [
   alki
 ];
 
-for (var c = 0; c < storeName.length; c++) {
-  storeName[c].render(storeName[c].sectionId);
-};
+renderTH();
+
+// for (var c = 0; c < storeName.length; c++) {
+//   storeName[c].render(storeName[c].sectionId);
+// };
